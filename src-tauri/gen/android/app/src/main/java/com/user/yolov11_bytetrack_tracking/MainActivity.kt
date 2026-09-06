1619086b8fadc7e117018d1ca1289491b39c4b5c
Nyi-Nyi-Myo <nyinyi.m@kkumail.com>
2026-09-06 23:42:00 +0700

    Commit files

---
+++ src-tauri/gen/android/app/src/main/java/com/user/yolov11_bytetrack_tracking/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.user.yolov11_bytetrack_tracking
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
